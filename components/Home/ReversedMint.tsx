1f5eb9d411e09d56dc148dff764cb5f463cb17a6
taayyohh <milesxbridges@gmail.com>
2022-12-13 10:31:46 -0500

    mint audio

---
+++ components/Home/ReversedMint.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import { ethers } from "ethers"
+import { useLayoutStore } from "../../stores/useLayoutStore"
+import { useContract } from "wagmi"
+import { ORIGIN_STORY_DROP } from "../../constants/addresses"
+import dropABI from "../../ABI/Drop.json"
+
+const ReservedMint = () => {
+  const { signer, provider } = useLayoutStore()
+  const contract = useContract({
+    address: "0xe44540c653558070d4acc0a4dc75de883f5e9df4",
+    abi: dropABI,
+    signerOrProvider: signer ?? provider,
+  })
+
+  const handleMint = React.useCallback(async () => {
+    if (!signer) {
+      //todo prompt connect
+
+      return
+    }
+
+    const { wait } = await contract?.purchase(1, { value: ethers.utils.parseEther(".01") }) //todo: make this not hard coded
+    await wait()
+    console.log("purchased")
+  }, [signer, contract])
+
+  return (
+    <div>
+      <div>
+        <div className={"mb-4 text-3xl hover:text-black"}>desrever flip 12.12.23</div>
+        <div className={"pb-6"}>Enjoying the sounds in the background. Mint :)</div>
+        <div className={"text-6xl font-bold"}>ITFO</div>
+        <div className={"p-12"}>
+          <img src={"https://zora-storage.imgix.net/bafybeicfkxtx4vxgaznggthgsb3hlmnjdhywuga3mot5rixdkiqod53zci"} />
+        </div>
+        <button
+          className="mt-4 flex w-full items-center justify-center rounded-xl border bg-black py-3 text-xl text-white hover:bg-white hover:text-black"
+          onClick={() => handleMint()}
+        >
+          Mint .01 ETH
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ReservedMint
